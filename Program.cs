1ce0561c71c7075dae9463bac3b73c22a6c1e140
Blaise <blazzze0517@gmail.com>
2024-03-16 22:49:56 -0400

    Added comments throughout current code

--- Program.cs
+++ Program.cs
@@ -11,30 +11,57 @@ namespace SimpleDiceGame
     {
         static void Main(string[] args)
         {
+            // Declare variables
+
+            // Creating human (you) container for dice roll
             int playerRandomNum;
+
+            // Creating AI container for dice roll 
             int enemyRandomNum;
+
+            // Starting human player points
             int playerPoints = 0;
+
+            // Starting AI points
             int enemyPoints = 0;
 
+            // Random number generator
             Random random = new Random();
+
+            // Game loop
+            // Runs 10 times
             for (int i = 0; i < 10; i++) {
+
+                // User input required for round to start
                 Console.WriteLine("Press any key to roll the dice.");
 
+                // Waiting for player input
                 Console.ReadKey();
 
+                // Calling random to get the human player's dice roll
                 playerRandomNum = random.Next(1, 7);
 
+                // Noting human player's dice roll value in console
                 Console.WriteLine("You rolled a " + playerRandomNum);
+
+                // Wait for 1/2 second
                 System.Threading.Thread.Sleep(500);
 
+                // Buffer
                 Console.WriteLine("...");
+
+                // Wait 1 second
                 System.Threading.Thread.Sleep(1000);
 
+                // Calling random to get the AI's dice roll
                 enemyRandomNum = random.Next(1, 7);
 
+                // Noting human AI's dice roll value in console
                 Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
                 System.Threading.Thread.Sleep(500);
 
+                // Outcome dependant on who rolled the highest number
+                // Draw if numbers rolled are the same
                 if (playerRandomNum > enemyRandomNum)
                 {
                     playerPoints++;
@@ -49,18 +76,24 @@ namespace SimpleDiceGame
                 {
                     Console.WriteLine("Draw. No one wins.");
                 }
-
+                
+                // Prints current score (factoring in last round outcome) for both human player and AI
                 Console.WriteLine("Score is now - Player: " + playerPoints + ", Enemy: " + enemyPoints + ".");
 
+                // Buffer
                 Console.WriteLine();
 
+                // Break
                 Console.WriteLine("--------------------------------");
-
+                
+                // Buffer
                 Console.WriteLine();
             }
 
+            // Buffer
             Console.WriteLine();
 
+            // Prints who wins based on score after 10 rounds 
             if (playerPoints > enemyPoints)
             {
                 Console.WriteLine("You win!");
@@ -74,6 +107,7 @@ namespace SimpleDiceGame
                 Console.WriteLine("It's a draw!");
             }
 
+            // Input needed from user to close program
             Console.ReadKey();
         }
     }
